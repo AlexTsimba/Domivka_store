f523771eccdadef4686d603673400798835bd14b
Alex Tsimba <alexander.tsimba@gmail.com>
2023-03-22 13:28:39 +0200

    add: automatic deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-ts-tailwind-boilerplate',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
 });
-
-if (process.env.NODE_ENV === 'production') {
-  ghPages.publish('dist', {
-    branch: 'deploy',
-    repo: 'git@github.com:AlexTsimba/React-ts-tailwind-boilerplate.git',
-  });
-}
