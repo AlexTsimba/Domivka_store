8d2ed054f198fe7865c27e64dee66afbd316e58f
Alex Tsimba <alexander.tsimba@gmail.com>
2023-03-22 12:35:18 +0200

    add production branch for automatic deployment to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ghPages from 'gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
+
+if (process.env.NODE_ENV === 'production') {
+  ghPages.publish('dist', {
+    branch: 'deploy',
+    repo: 'git@github.com:AlexTsimba/React-ts-tailwind-boilerplate.git',
+  });
+}
